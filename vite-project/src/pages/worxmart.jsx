bdd8fb3e0ee88f088f2bde39c452616f90bcc14d
shehabMm <shehab.mm@gmail.com>
2023-04-11 04:24:25 +0200

    f

--- vite-project/src/pages/worxmart.jsx
+++ vite-project/src/pages/worxmart.jsx
@@ -1,153 +1,141 @@
 import React from "react";
 import "./worxmart.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import { Button } from "@mui/material";
-import Statics from "../components/statics";
+import Wiper from "../components/wiper";
 
 const Worxmart = () => {
-
-
-  const [age, setAge] = React.useState('');
+  const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
 
   return (
-
-    <>
-    <div className="whole">
-      <div className="container1">
-        <nav>
-          <ul>
-            <li>
-              <button className="myButton login">Login</button>
-            </li>
-            <li>
-              <button className="myButton">Register</button>
-            </li>
-            <li>
-              <button className="btnMob">Start</button>
-
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className="between">
-        <p> Explore Services </p>
-        <p>Explore Requeists</p>
-        <p>How it works</p>{" "}
-      </div>
-
-      <div className="logo">
-        <img
-          width="200px"
-          height="100px"
-          src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312330/shehab/SL-111719-25140-24-removebg-preview_chrjqn.png"
-          alt=""
-        />
-      </div>
-      <MenuIcon
-        className="menuBurger"
-        sx={{
-          fontSize: "40px",
-          top: "30px",
-          position: "fixed",
-          ml: 2,
-          display: { md: "none", xs: "block" },
-          cursor: "pointer"
-        }}
-      />
-
-      <div className="mainContent">
-
-
-        <h1>Hire a professional Talent <br />
-          to get your job done</h1>
-        <p>Find top talens, fast and easy. Connect with the <br />
-          best freelancers and bring your projects to life.</p>
-
-        <FormControl className="select" sx={{ backgroundColor: "white", border: "2px solid yellow" }} >
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-          <Button>search</Button>
-
-        </FormControl>
-
-
-
-
-
-
-        <div className="maingImg">
-
-          <img width="400px" src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681082511/user_aowxl1.png" alt="" />
+    <div className="body">
+      <div className="whole">
+        <div className="container1">
+          <nav>
+            <ul>
+              <li>
+                <button className="myButton login">Login</button>
+              </li>
+              <li>
+                <button className="myButton">Register</button>
+              </li>
+              <li>
+                <button className="btnMob">Start</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="between">
+          <p> Explore Services </p>
+          <p>Explore Requeists</p>
+          <p>How it works</p>{" "}
         </div>
 
+        <div className="logo">
+          <img
+            width="200px"
+            height="100px"
+            src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1676312330/shehab/SL-111719-25140-24-removebg-preview_chrjqn.png"
+            alt=""
+          />
+        </div>
+        <MenuIcon
+          className="menuBurger"
+          sx={{
+            fontSize: "40px",
+            top: "30px",
+            position: "fixed",
+            ml: 2,
+            display: { md: "none", xs: "block" },
+            cursor: "pointer",
+          }}
+        />
+
+        <div className="mainContent">
+          <h1>
+            Hire a professional Talent <br />
+            to get your job done
+          </h1>
+          <p>
+            Find top talens, fast and easy. Connect with the <br />
+            best freelancers and bring your projects to life.
+          </p>
+
+          <FormControl
+            className="select"
+            sx={{ backgroundColor: "white", border: "2px solid yellow" }}
+          >
+            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            <Button>search</Button>
+          </FormControl>
+
+          <div className="maingImg">
+            <img
+              width="400px"
+              src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681082511/user_aowxl1.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="bar">
+          <img
+            src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/nsnlogo-1_ias9r0.png"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/MZsolutionlogo-1_kpifty.png"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/Fut-trans-logo-1_zfb1zx.png"
+            alt=""
+          />
+          <img
+            src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/WhatsApp-Image-2023-02-06-at-1.04-1-1_bujhil.png"
+            alt=""
+          />
+        </div>
+
+        <div className="footer">
+          <p>
+            + 16,000 <br /> Registered talents
+          </p>
+
+          <p>
+            Up to 50% <br /> Cost saving
+          </p>
+
+          <p>
+            + 200 <br /> Categories
+          </p>
+        </div>
       </div>
-
-      <div className="bar">
-        <img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/nsnlogo-1_ias9r0.png" alt="" />
-        <img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/MZsolutionlogo-1_kpifty.png" alt="" />
-        <img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/Fut-trans-logo-1_zfb1zx.png" alt="" />
-        <img src="https://res.cloudinary.com/dvytkrzaq/image/upload/v1681154825/WhatsApp-Image-2023-02-06-at-1.04-1-1_bujhil.png" alt="" />
-
+      <div className="newSection">
+        <Wiper />
       </div>
-
-
-
-
-      <div className="footer">
-
-        <p>+ 16,000 <br /> Registered talents</p>
-
-        <p>Up to 50% <br /> Cost saving</p>
-
-        <p>+ 200 <br /> Categories</p>
-
-      </div>
-
-
-
-
-
-
     </div>
-    <div className="newSection">
-
-<p>fffffffffffffffffff</p>
-
-<p></p>
-
-<p></p>
-
-
-
-
-
-</div>
-
-</>
-
-
   );
-
-
 };
 
 export default Worxmart;
