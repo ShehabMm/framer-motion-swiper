d40ba7d01cd76d1caa9d357773b82d57c1cc4d68
shehabMm <shehab.mm@gmail.com>
2023-04-22 08:57:14 +0200

    morgan

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const dotenv = require('dotenv');
+const morgan =require('morgan')
 const app = express();
 dotenv.config({path:'config.env'})
+
+
+if(process.env.NODE_ENV == 'development'){
+  app.use(morgan("dev"));
+console.log('mode: ${process.env.NODE_ENV}');
+}
+
 app.get("/", (req, res) => {
   res.send("API v2");
 });
