4155e53d11779ea3dc506c58f83d49ef45aeb43a
shehabMm <shehab.mm@gmail.com>
2023-04-22 09:51:59 +0200

    mo

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const morgan =require('morgan')
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require('mongoose');
 const app = express();
-dotenv.config({path:'config.env'})
+dotenv.config({ path: "config.env" });
 
+//connect to db
 
-if(process.env.NODE_ENV == 'development'){
+mongoose.connect(process.env.DB_URI).then((conn) => {
+  console.log(`Database conncted: ${conn.conneciton.host}`)
+
+}).catch((err) => {
+  console.log(`Database Error: ${err}`)
+process.exit(1)
+})
+
+if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
-console.log('mode: ${process.env.NODE_ENV}');
+  console.log(`mode:${process.env.NODE_ENV}`);
 }
 
 app.get("/", (req, res) => {
   res.send("API v2");
 });
 
-
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT} `)
-})
-
-
-
-
-
+  console.log(`App running on port ${PORT} `);
+});
